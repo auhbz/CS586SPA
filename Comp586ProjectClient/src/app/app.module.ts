f15805bd95b91302840dbf09fdf6c79d6f5f4ca2
auhbz <auhbon@Auhbons-MBP.lan>
2020-12-05 16:28:04 -0800

    generated login and logout button components

--- Comp586ProjectClient/src/app/app.module.ts
+++ Comp586ProjectClient/src/app/app.module.ts
@@ -3,18 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BoardGamesComponent } from './components/board-games/board-games.component';
 
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment as env } from '../environments/environment';
+import { LoginButtonComponent } from './components/login-button/login-button.component';
+import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
 @NgModule({
   declarations: [
     AppComponent,
-    BoardGamesComponent
+    BoardGamesComponent,
+    LoginButtonComponent,
+    LogoutButtonComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AuthModule.forRoot({
+      ...env.auth,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
